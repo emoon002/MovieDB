f066a7a572dc4ac3abe52bf44710025e39db5112
emoon002 <emoon002@gmail.com>
2022-11-07 14:01:20 -0800

    Updated API URL to use https

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import MovieCard from './MovieCard';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com/?apikey=63dfdd35'
+const API_URL = 'https://www.omdbapi.com/?apikey=63dfdd35'
 
 const App = () => {
 
